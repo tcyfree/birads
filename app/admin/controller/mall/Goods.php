d543bf40683c6a421ed3e1f4e030602ceab77dd9
tangcy <bohr@icloud.com>
2023-06-12 09:51:31 +0800

    birads3

--- app/admin/controller/mall/Goods.php
+++ app/admin/controller/mall/Goods.php
@@ -98,50 +98,24 @@ class Goods extends AdminController
     public function edit($id)
     {
         $row = $this->model->find($id);
-        empty($row) && $this->error('数据不存在');
+        empty($row) && $this->error('Data does not exist');
         if ($this->request->isPost()) {
             $post = $this->request->post();
            
              $rule = [
-                'logo'  => 'require|between:1,2',
-                'sales'   => 'require|between:1,2'
+                'logo'  => 'require|between:1,2'
             ];
 
     $message = [
-      'logo.between' => 'First Identification Required',
-      'sales.between' => 'Second Indentification Required',
+      'logo.between' => 'Identification Required',
     ];
             $this->validate($post, $rule,$message);
             try {
                 $save = $row->save($post);
             } catch (\Exception $e) {
-                $this->error('保存失败');
+                $this->error('Save failed');
             }
-            $save ? $this->success('保存成功') : $this->error('保存失败');
-        }
-        $this->assign('row', $row);
-        return $this->fetch();
-    }
-
-    /**
-     * @NodeAnotation(title="入库")
-     */
-    public function stock($id)
-    {
-        $row = $this->model->find($id);
-        empty($row) && $this->error('数据不存在');
-        if ($this->request->isPost()) {
-            $post = $this->request->post();
-            $rule = [];
-            $this->validate($post, $rule);
-            try {
-                $post['total_stock'] = $row->total_stock + $post['stock'];
-                $post['stock'] = $row->stock + $post['stock'];
-                $save = $row->save($post);
-            } catch (\Exception $e) {
-                $this->error('保存失败');
-            }
-            $save ? $this->success('保存成功') : $this->error('保存失败');
+            $save ? $this->success('Successfully saved') : $this->error('Save failed');
         }
         $this->assign('row', $row);
         return $this->fetch();
